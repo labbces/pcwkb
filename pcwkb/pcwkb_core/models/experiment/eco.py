2b157a3725b133ed7a2ea5afba104b74d26939d4
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-09-28 10:46:22 -0300

    created eco model

---
+++ pcwkb/pcwkb_core/models/experiment/eco.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class ECOTerm(models.Model):
+    eco_id = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_values_as_dict(self):
+        new_dict = {
+            'eco_id' : self.eco_id,
+            'name' : self.name,
+            'description': self.description,
+            'model_name' : 'ECOTerm',
+        }
+        return new_dict
