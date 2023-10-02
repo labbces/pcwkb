b13fee216ce4801ad287e051ada2e9382d42afcc
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-10-02 15:44:01 -0300

    changed folder

--- pcwkb/pcwkb_core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from pcwkb_core.views import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("browse_species", views.browse_species, name='browse_species'),
-]
