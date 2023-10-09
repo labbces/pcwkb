e36fd9ce43b3e8de6c523a64614fdabe4f6e91b0
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-10-09 15:40:20 -0300

    included a function to get data from db

--- pcwkb/pcwkb_core/views/views.py
+++ pcwkb/pcwkb_core/views/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.shortcuts import render
+from pcwkb_core.models.taxonomy.ncbi_taxonomy import Species
 
 
 def index(request):
@@ -15,3 +16,11 @@ def browse_species(request):
     
 
     return render(request, 'species/browse_species.html')
+
+def species_page(request):
+
+    context = {'species_name': 'Oriza sativa',
+                'common_name': ''}
+    context['common_name'] = Species.objects.get(species_name=context['species_name']).common_name
+    
+    return render(request, 'species/species_page.html', context)
