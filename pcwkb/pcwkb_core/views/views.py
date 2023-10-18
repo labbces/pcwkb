1285b11c0c10e519882d64212cd0cb555cfd8465
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-10-18 09:32:39 -0300

    Added new ppages to views.py

--- pcwkb/pcwkb_core/views/views.py
+++ pcwkb/pcwkb_core/views/views.py
@@ -11,16 +11,28 @@ from pcwkb_core.models.taxonomy.ncbi_taxonomy import Species
 def index(request):
     return render(request, 'home.html')
 
+def faq(request):
+    return render(request, 'faq.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def funding(request):
+    return render(request, 'funding.html')
+
 def browse_species(request):
 
     
 
     return render(request, 'species/browse_species.html')
 
-def species_page(request):
+def species_page(request, species_code):
 
-    context = {'species_name': 'Oriza sativa',
+    context = { 'species_code': species_code,
+                'scientific_name': '',
                 'common_name': ''}
-    context['common_name'] = Species.objects.get(species_name=context['species_name']).common_name
+
+    context['common_name'] = Species.objects.get(species_code=species_code).common_name
+    context['scientific_name'] = Species.objects.get(species_code=species_code).scientific_name
     
     return render(request, 'species/species_page.html', context)
