f5e2cacf0811b419170bdaff3858298269ef0df9
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-10-18 09:32:13 -0300

    Adde new pages path to the urls.py

--- pcwkb/pcwkb_core/urls/urls.py
+++ pcwkb/pcwkb_core/urls/urls.py
@@ -5,5 +5,8 @@ from pcwkb_core.views import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("browse_species", views.browse_species, name='browse_species'),
-    path("species_page", views.species_page, name='species_page'),
+    path("species_page/<str:species_code>", views.species_page, name='species_page'),
+    path('team', views.team, name='team'),
+    path('funding', views.funding, name='funding'),
+    path('faq', views.faq, name='faq'),
 ]
