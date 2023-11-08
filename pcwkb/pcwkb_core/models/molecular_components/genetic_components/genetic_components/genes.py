7f8c3b165390b7ba639c79c0ec179e0ee693fcc1
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-11-08 07:26:43 -0300

    added genes

--- pcwkb/pcwkb_core/models/molecular_components/genetic_components/genetic_components/genes.py
+++ pcwkb/pcwkb_core/models/molecular_components/genetic_components/genetic_components/genes.py
@@ -3,18 +3,18 @@ from .models import GeneOntologyTerm
 
 class Gene(models.Model):
     gene_id = models.CharField(max_length=50, unique=True)
-    name = models.CharField(max_length=100)
+    gene_name = models.CharField(max_length=100)
     description = models.TextField()('self', on_delete=models.SET_NULL, null=True, blank=True)
     original_db = models.CharField(max_length=50, unique=True)
     go = models.ForeignKey(GeneOntologyTerm, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.gene_name
 
     def get_values_as_dict(self):
         new_dict = {
             'gene_id' : self.gene_id,
-            'name' : self.name,
+            'name' : self.gene_name,
             'description': self.description,
             'original_db': self.original_db,
             'go': self.go 
