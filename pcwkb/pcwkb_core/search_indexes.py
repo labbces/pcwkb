225f8bc54f2a8281b92d293472c34bf5331e92b2
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-11-08 07:26:11 -0300

    included other fields to the SpeciesIndex class

--- pcwkb/pcwkb_core/search_indexes.py
+++ pcwkb/pcwkb_core/search_indexes.py
@@ -3,9 +3,20 @@ from .models.taxonomy.ncbi_taxonomy import Species
 
 class SpeciesIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    species_code = indexes.CharField(model_attr='species_code')
     scientific_name = indexes.CharField(model_attr='scientific_name')
-    common_name = indexes.CharField(model_attr='common_name')
+    common_name = indexes.CharField(model_attr='common_name', null=True)
+    family = indexes.CharField(model_attr='family')
+    clade = indexes.CharField(model_attr='clade')
+    photosystem = indexes.CharField(model_attr=' photosystem')
+
+    # Autocomplete
+    content_auto = indexes.EdgeNgramField(model_attr='scientific_name')
+    content_auto2 = indexes.EdgeNgramField(model_attr='common_name', null=True)
 
     def get_model(self):
         return Species
 
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
