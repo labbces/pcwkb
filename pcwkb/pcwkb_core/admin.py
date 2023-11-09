45cc6c64f0608ee9ac9100ba0148ab949150eed0
Renato Augusto Correa dos Santos <renatoacsantos@gmail.com>
2023-11-09 14:15:15 -0300

    Registered model in admin.py

--- pcwkb/pcwkb_core/admin.py
+++ pcwkb/pcwkb_core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from .models.taxonomy.ncbi_taxonomy import Species
+from .models.molecular_components.genetic_components.genes import Gene
 
 admin.site.register(Species)
+admin.site.register(Gene)
