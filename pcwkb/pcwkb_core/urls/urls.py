846bf77ccbe6c4f1500bf278472f332f880f2075
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-12-14 15:01:43 -0300

    changed path to match the modules

--- pcwkb/pcwkb_core/urls/urls.py
+++ pcwkb/pcwkb_core/urls/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from django.urls import path, include
 
-from pcwkb.pcwkb_core.views.taxonomy import species
-from pcwkb.pcwkb_core.views.molecular_components import gene
+from pcwkb_core.views import base_views
+from pcwkb_core.views.taxonomy import species
+from pcwkb_core.views.molecular_components import gene
 
 urlpatterns = [
-    path("", species.index, name="index"),
     path("browse_species", species.browse_species, name='browse_species'),
     path("species_page/<str:species_code>", species.species_page, name='species_page'),
     path("gene_page/<int:gene_id>", gene.gene_page, name='gene_page'),
-    path('team', species.team, name='team'),
-    path('funding', species.funding, name='funding'),
-    path('faq', species.faq, name='faq'),
+    path('', base_views.index, name='index'),
+    path('team', base_views.team, name='team'),
+    path('funding', base_views.funding, name='funding'),
+    path('faq', base_views.faq, name='faq'),
     path('search/search', include('haystack.urls')),
 ]
--- pcwkb/pcwkb_core/views/molecular_components/gene.py
+++ pcwkb/pcwkb_core/views/molecular_components/gene.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 
 from pcwkb_core.models.molecular_components.genetic.genes import Gene
-from pcwkb.pcwkb_core.models.functional_annotation.experimental.relationships.gene_experiment_association import GeneExperimentAssociation
+from pcwkb_core.models.functional_annotation.experimental.relationships.gene_experiment_association import GeneExperimentAssociation
 
 def gene_page(request, gene_id):
+    
+    gene = Gene.objects.get(id=gene_id)
 
     context = {'gene_id': gene_id,
-               'gene_name': '',
-               'description': ''}
+               'gene_name': gene.gene_name,
+               'description': gene.description}
     
     return render(request, 'gene/gene_page.html', context)
