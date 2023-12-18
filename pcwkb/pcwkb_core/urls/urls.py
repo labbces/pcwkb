5a4edba613301b49e8bc1e7ab37cbebbc22c649f
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-12-18 13:24:07 -0300

    created forms for data_submission

--- pcwkb/pcwkb_core/urls/urls.py
+++ pcwkb/pcwkb_core/urls/urls.py
@@ -4,14 +4,16 @@ from django.urls import path, include
 from pcwkb_core.views import base_views
 from pcwkb_core.views.taxonomy import species
 from pcwkb_core.views.molecular_components import gene
+from pcwkb_core.views.forms import data_submission
 
 urlpatterns = [
-    path("browse_species", species.browse_species, name='browse_species'),
-    path("species_page/<str:species_code>", species.species_page, name='species_page'),
-    path("gene_page/<int:gene_id>", gene.gene_page, name='gene_page'),
+    path('browse_species', species.browse_species, name='browse_species'),
+    path('species_page/<str:species_code>', species.species_page, name='species_page'),
+    path('gene_page/<int:gene_id>', gene.gene_page, name='gene_page'),
     path('', base_views.index, name='index'),
     path('team', base_views.team, name='team'),
     path('funding', base_views.funding, name='funding'),
     path('faq', base_views.faq, name='faq'),
     path('search/search', include('haystack.urls')),
+    path('data_submission', data_submission.index, name='data_submission')
 ]
