8a3775d9b6ba8b6dba5073902c84ea9eaf216f3e
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-12-20 13:05:50 -0300

    added forms and ontology parser

--- pcwkb/pcwkb_core/models/functional_annotation/computational/gene_ontology.py
+++ pcwkb/pcwkb_core/models/functional_annotation/computational/gene_ontology.py
@@ -11,55 +11,51 @@ class GeneOntologyTerm(models.Model):
     def __str__(self):
         return self.go_term
 
-    def add_from_obo(filename, empty=True, compressed=False):
-        """
-        Parses GeneOntology's OBO file and adds it to the database
+def add_from_obo(filename, empty=True, compressed=False):
+    """
+    Parses GeneOntology's OBO file and adds it to the database
+    :param filename: Path to the OBO file to parse
+    :param compressed: load data from .gz file if true (default: False)
+    :param empty: Empty the database first when true (default: True)
+    """
+    # If required empty the table first
+    # TODO: verificar se o banco está vazio antes de colocar os termos do GO
 
-        :param filename: Path to the OBO file to parse
-        :param compressed: load data from .gz file if true (default: False)
-        :param empty: Empty the database first when true (default: True)
-        """
-        # If required empty the table first
-        # TODO: verificar se o banco está vazio antes de colocar os termos do GO
+    obo_parser = OBOParser()
+    obo_parser.readfile(filename, compressed=compressed)
+    obo_parser.extend_go()
 
-        obo_parser = OBOParser()
-        obo_parser.readfile(filename, compressed=compressed)
-        obo_parser.extend_go()
+    if empty:
+        GeneOntologyTerm.objects.all().delete()
 
-        if empty:
-            # Assuming your model is named GO and is in your_django_app.models
-            GO.objects.all().delete()
+    for i, term in enumerate(obo_parser.terms):
+        go = GeneOntologyTerm(
+            term_id=term.id,
+            name=term.name,
+            namespace=term.namespace,
+            definition=term.definition,
+            is_obsolete=term.is_obsolete,
+            is_a=";".join(term.is_a),
+            extended_go=";".join(term.extended_go),
+        )
 
-        for i, term in enumerate(obo_parser.terms):
-            go = GO(
-                term_id=term.id,
-                name=term.name,
-                namespace=term.namespace,
-                definition=term.definition,
-                is_obsolete=term.is_obsolete,
-                is_a=";".join(term.is_a),
-                extended_go=";".join(term.extended_go),
-            )
+        go.save()
 
-            go.save()
+        if i % 40 == 0:
+            # commit to the db frequently to allow indexing functions to work without timing out
+            try:
+                # Assuming you are using Django ORM
+                transaction.commit()
+            except Exception as e:
+                transaction.rollback()
+                print(e)
 
-            if i % 40 == 0:
-                # commit to the db frequently to allow indexing functions to work without timing out
-                try:
-                    # Assuming you are using Django ORM
-                    transaction.commit()
-                except Exception as e:
-                    transaction.rollback()
-                    print(e)
-
-        try:
-            # commit remaining changes
-            transaction.commit()
-        except Exception as e:
-            transaction.rollback()
-            print(e)
-
-
+    try:
+        # commit remaining changes
+        transaction.commit()
+    except Exception as e:
+        transaction.rollback()
+        print(e)
     
 
 class GOProteinAssociation(models.Model):
