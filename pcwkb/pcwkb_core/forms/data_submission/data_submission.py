aa05686a814d519a822823f63829069efa7d8912
Bianca Sagiorato <biancasagiorato@estudante.ufscar.br>
2023-12-21 09:02:11 -0300

    added the fields to the data submission form

--- pcwkb/pcwkb_core/forms/data_submission/data_submission.py
+++ pcwkb/pcwkb_core/forms/data_submission/data_submission.py
@@ -1,3 +1,19 @@
-from django.db import forms
+from django import forms
 
-# Create your forms here.
+
+class DataSubmissionForm(forms.Form):
+    """ Class to submit 
+
+
+    """
+    title = forms.CharField(max_length=50)
+    #input_file = forms.FileField()
+
+
+class SpeciesSubmissionForm(forms.Form):
+    """
+    """
+    taxid = forms.IntegerField(label="Plant TaxID")
+    plant_image = forms.ImageField(label="Plant image")
+    plant_image_source = forms.URLField(label="Image Source URL", required=True)
+    description = forms.CharField(label="Plant description", max_length=200)
