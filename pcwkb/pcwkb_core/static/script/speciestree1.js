89f9c1bdfe66338fac7d890de72c45b4e94eaae3
JNovoletti <joao.novoletti@gmail.com>
2024-03-11 11:26:20 -0300

    changing the speciestree text display

--- pcwkb/pcwkb_core/static/script/speciestree1.js
+++ pcwkb/pcwkb_core/static/script/speciestree1.js
@@ -1,134 +1,144 @@
+// Collapsible tree based on https://observablehq.com/@d3/collapsible-tree by Mike Bostock
+
 console.log(data);
 
-    const width = 1000;
-    const marginTop = 100;
-    const marginRight = 100;
-    const marginBottom = 100;
-    const marginLeft = 300;
-    
-    const dx = 60;
-    const dy = 120;
-    
-    const tree = d3.tree().nodeSize([dx, dy]);
-    const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
-    
-    const svg = d3.create("svg")
-        .attr("width", width)
-        .attr("height", dy)
-        .attr("viewBox", [-marginLeft, -marginTop, width, dy])
-        .attr("style", "max-width: 100%; height: auto; font: 20px sans-serif; user-select: none;");
-    
-    const gLink = svg.append("g")
-        .attr("fill", "none")
-        .attr("stroke", "#008000")
-        .attr("stroke-opacity", 0.4)
-        .attr("stroke-width", 1.5);
-    
-    const gNode = svg.append("g")
-        .attr("cursor", "pointer")
-        .attr("pointer-events", "all");
-    
-    function update(event, source) {
-      const duration = event?.altKey ? 2500 : 250;
-      const nodes = root.descendants().reverse();
-      const links = root.links();
-    
-      tree(root);
-    
-      let left = root;
-      let right = root;
-      root.eachBefore(node => {
-        if (node.x < left.x) left = node;
-        if (node.x > right.x) right = node;
-      });
-    
-      const height = right.x - left.x + marginTop + marginBottom;
-    
-      const transition = svg.transition()
-          .duration(duration)
-          .attr("height", height)
-          .attr("viewBox", [-marginLeft, left.x - marginTop, width, height])
-          .tween("resize", window.ResizeObserver ? null : () => () => svg.dispatch("toggle"));
-    
-      const node = gNode.selectAll("g")
-        .data(nodes, d => d.id);
-    
-      const nodeEnter = node.enter().append("g")
-          .attr("transform", d => `translate(${source.y0},${source.x0})`)
-          .attr("fill-opacity", 0)
-          .attr("stroke-opacity", 0)
-          .on("click", (event, d) => {
-            d.children = d.children ? null : d._children;
-            update(event, d);
-          });
-    
-      nodeEnter.append("circle")
-          .attr("r", 2.5)
-          .attr("fill", d => d._children ? "#555" : "#999")
-          .attr("stroke-width", 10);
-    
-    nodeEnter.append("text")
-        .attr("dy", "0.31em")
-        .attr("x", d => d._children ? -6 : 6)
-        .attr("text-anchor", d => d._children ? "end" : "start")
-        .each(function(d) {
-          if (d.data.name.match(/\((.*?)\)/)?.[1]) {
-              d3.select(this).append("a")
-                  .attr("href", d => {
-                      return "http://127.0.0.1:8000/pcwkb_core/species_page/" + d.data.name.match(/\((.*?)\)/)?.[1];
-                  })
-                  .attr("target", "_blank")
-                      .text(d => d.data.name)
-          }
-          else {
-              d3.select(this).append("tspan")
-                  .text(d => d.data.name)
-          }
-      });        
-    
-      const nodeUpdate = node.merge(nodeEnter).transition(transition)
-          .attr("transform", d => `translate(${d.y},${d.x})`)
-          .attr("fill-opacity", 1)
-          .attr("stroke-opacity", 1);
-    
-      const nodeExit = node.exit().transition(transition).remove()
-          .attr("transform", d => `translate(${source.y},${source.x})`)
-          .attr("fill-opacity", 0)
-          .attr("stroke-opacity", 0);
-    
-      const link = gLink.selectAll("path")
-        .data(links, d => d.target.id);
-    
-      const linkEnter = link.enter().append("path")
-          .attr("d", d => {
-            const o = {x: source.x0, y: source.y0};
-            return diagonal({source: o, target: o});
-          });
-    
-      link.merge(linkEnter).transition(transition)
-          .attr("d", diagonal);
-    
-      link.exit().transition(transition).remove()
-          .attr("d", d => {
-            const o = {x: source.x, y: source.y};
-            return diagonal({source: o, target: o});
-          });
-    
-      root.eachBefore(d => {
-        d.x0 = d.x;
-        d.y0 = d.y;
-      });
-    }
-    
-    const root = d3.hierarchy(data);
-    root.x0 = dy / 2;
-    root.y0 = 0;
-    root.descendants().forEach((d, i) => {
-      d.id = i;
-      d._children = d.children;
-      if (d.depth && d.data.name.length !== 2) d.children = null;
+const width = 1000;
+const marginTop = 100;
+const marginRight = 100;
+const marginBottom = 100;
+const marginLeft = 300;
+
+const root = d3.hierarchy(data);
+const dx = 60;
+const dy = (width - marginRight - marginLeft) / (1 + root.height);
+
+const tree = d3.tree().nodeSize([dx, dy]);
+const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
+
+const svg = d3.create("svg")
+  .attr("width", width)
+  .attr("height", dy)
+  .attr("viewBox", [-marginLeft, -marginTop, width, dy])
+  .attr("style", "max-width: 100%; height: auto; font: 20px sans-serif; user-select: none;");
+
+const gLink = svg.append("g")
+  .attr("fill", "none")
+  .attr("stroke", "#008000")
+  .attr("stroke-opacity", 0.4)
+  .attr("stroke-width", 1.5);
+
+const gNode = svg.append("g")
+  .attr("cursor", "pointer")
+  .attr("pointer-events", "all");
+
+function update(event, source) {
+  const duration = event?.altKey ? 2500 : 250;
+  const nodes = root.descendants().reverse();
+  const links = root.links();
+
+  tree(root);
+
+  let left = root;
+  let right = root;
+  root.eachBefore(node => {
+    if (node.x < left.x) left = node;
+    if (node.x > right.x) right = node;
+  });
+
+  const height = right.x - left.x + marginTop + marginBottom;
+
+  const transition = svg.transition()
+    .duration(duration)
+    .attr("height", height)
+    .attr("viewBox", [-marginLeft, left.x - marginTop, width, height])
+    .tween("resize", window.ResizeObserver ? null : () => () => svg.dispatch("toggle"));
+
+  const node = gNode.selectAll("g")
+    .data(nodes, d => d.id);
+
+  const nodeEnter = node.enter().append("g")
+    .attr("transform", d => `translate(${source.y0},${source.x0})`)
+    .attr("fill-opacity", 0)
+    .attr("stroke-opacity", 0)
+    .on("click", (event, d) => {
+      d.children = d.children ? null : d._children;
+      update(event, d);
     });
-    
-    update(null, root);
-    
-    document.body.appendChild(svg.node());
+
+  nodeEnter.append("circle")
+    .attr("r", 4)
+    .attr("fill", d => d._children ? "#555" : "#999")
+    .attr("stroke-width", 10);
+
+  nodeEnter.append("text")
+    .attr("dy", "0.31em")
+    .attr("x", d => d._children ? -6 : 6)
+    .attr("text-anchor", d => d._children ? "end" : "start")
+    .each(function (d) {
+      if (d.data.name.match(/\((.*?)\)/)?.[1]) {
+        d3.select(this).append("a")
+          .attr("href", d => {
+            return "http://127.0.0.1:8000/pcwkb_core/species_page/" + d.data.name.match(/\((.*?)\)/)?.[1];
+          })
+          .attr("target", "_blank")
+          .text(d => d.data.name)
+          .attr("stroke-linejoin", "round")
+          .attr("stroke-width", 3)
+          .attr("stroke", "white")
+          .attr("paint-order", "stroke");
+      }
+      else {
+        d3.select(this).append("tspan")
+          .text(d => d.data.name)
+          .attr("stroke-linejoin", "round")
+          .attr("stroke-width", 6)
+          .attr("stroke", "white")
+          .attr("paint-order", "stroke");
+      }
+    });
+
+  const nodeUpdate = node.merge(nodeEnter).transition(transition)
+    .attr("transform", d => `translate(${d.y},${d.x})`)
+    .attr("fill-opacity", 1)
+    .attr("stroke-opacity", 1);
+
+  const nodeExit = node.exit().transition(transition).remove()
+    .attr("transform", d => `translate(${source.y},${source.x})`)
+    .attr("fill-opacity", 0)
+    .attr("stroke-opacity", 0);
+
+  const link = gLink.selectAll("path")
+    .data(links, d => d.target.id);
+
+  const linkEnter = link.enter().append("path")
+    .attr("d", d => {
+      const o = { x: source.x0, y: source.y0 };
+      return diagonal({ source: o, target: o });
+    });
+
+  link.merge(linkEnter).transition(transition)
+    .attr("d", diagonal);
+
+  link.exit().transition(transition).remove()
+    .attr("d", d => {
+      const o = { x: source.x, y: source.y };
+      return diagonal({ source: o, target: o });
+    });
+
+  root.eachBefore(d => {
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+}
+
+root.x0 = dy / 2;
+root.y0 = 0;
+root.descendants().forEach((d, i) => {
+  d.id = i;
+  d._children = d.children;
+  if (d !== root) d.children = null;
+});
+
+update(null, root);
+
+document.body.appendChild(svg.node());
