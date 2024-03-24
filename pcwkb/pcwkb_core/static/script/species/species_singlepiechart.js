2cf31f1c642f3bd4e909da89a80fc60dd5786550
JNovoletti <joao.novoletti@gmail.com>
2024-03-23 22:11:23 -0300

    developing and adjusting pie chart and heatmap

--- pcwkb/pcwkb_core/static/script/species/species_singlepiechart.js
+++ pcwkb/pcwkb_core/static/script/species/species_singlepiechart.js
@@ -4,36 +4,34 @@ console.log(scientificName);
 console.log(biomasscomp);
 const PLOT = document.getElementById('grafico');
 console.log(PLOT)
-if (PLOT) {
 
-  var layout = {
-    height: 400,
-    width: 400,
+var layout = {
+  title: "Biomass composition pie chart for <br> PO: " + PO,
+};
+
+for (var part in biomasscomp) {
+  console.log(part)
+  var values = [];
+  var labels = [];
+  for (var component in biomasscomp[part]) {
+    console.log(component, biomasscomp[part][component])
+    labels.push(component);
+    values.push(biomasscomp[part][component]);
   }
-
-  for (var part in biomasscomp) {
-    console.log(part)
-    var values = [];
-    var labels = [];
-    for (var component in biomasscomp[part]) {
-      console.log(component,biomasscomp[part][component])
-      labels.push(component);
-      values.push(biomasscomp[part][component]);
-    }
-  }
-  
-  var data = [{
-    type: "pie",
-    values: values,
-    labels: labels,
-    hoverinfo: "label+percent",
-    textinfo: "label+percent",
-    textposition: "outside",
-    automargin: true,
-    }]
-
-  Plotly.newPlot(PLOT, data, layout);
 }
 
+var data = [{
+  type: "pie",
+  values: values,
+  labels: labels,
+  hoverinfo: "label+percent",
+  textinfo: "label+percent",
+  textposition: "outside",
+  automargin: true,
+}]
+
+var config = {responsive: true}
+
+Plotly.newPlot(PLOT, data, layout, config);
+
 console.log(layout)
-console.log(pieChartData)
