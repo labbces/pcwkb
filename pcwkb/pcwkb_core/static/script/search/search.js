3f4051095ff056504c59deb70069171230cac89b
JNovoletti <joao.novoletti@gmail.com>
2024-05-20 17:11:14 -0300

    search related update

--- pcwkb/pcwkb_core/static/script/search/search.js
+++ pcwkb/pcwkb_core/static/script/search/search.js
@@ -4,33 +4,56 @@ $(document).ready(function () {
 
         var query = $('#search-input').val().trim();
         if (query.length > 0) {
+            $('#search-results').html('<p>Loading...</p>'); // Loading indicator
+
             // Manually trigger AJAX request to search_pcwkb endpoint
             $.ajax({
                 url: '/pcwkb_core/search_pcwkb/',
                 method: 'GET',
                 data: { 'q': query },
                 success: function (response) {
-                    // Clear previous results
+                    // Clear previous data
                     $('#search-results').empty();
-                    // Display species names
-                    $('#search-results').html('<p>Search result: <a href="' + response.results.url + '" target="_blank">' + response.results.label + '</a></p>');
-                    // Update object data
-                    updateObjectData(response);
+                    $('#species-data').empty();
+                    $('#gene-data').empty();
+
+                    let content = '';
+                    if (response.results && response.results.length > 0) {
+                        content = '<p>Search results:</p>';
+                        response.results.forEach(function(result) {
+                            content += '<p><a href="' + result.url_species + '" target="_blank">' + result.label + '</a></p>';
+                            updateResponseDivs(result);
+                        });
+                    } else {
+                        content = '<p>Search result: No results found.</p>';
+                    }
+                    $('#search-results').html(content);
                 },
                 error: function (xhr, status, error) {
-                    // Handle error
-                    console.error(xhr.responseText);
+                    $('#search-results').empty(); // Clear previous results
+                    $('#search-results').html('<p>Search result: An error occurred. Please try again later.</p>');
+                    console.error(`Error: ${error}`);
                 }
             });
         }
     });
 
-    function updateObjectData(response) {
+    function updateResponseDivs(result) {
+        // Compile templates
+        var speciesTemplate = _.template($('#species-card-template').html());
+        var geneTemplate = _.template($('#gene-card-template').html());
+
         // Update the HTML with the objectData content
-        var speciesHtml = `<p>Species: ${response.results.specie}</p>`;
-        var geneHtml = `<p>Gene: ${response.results.genes}</p>`;
+        var speciesHtml = speciesTemplate({ specie: '<a href="' + result.url_species + '" target="_blank">' + result.specie + '</a></p>' });
         
-        $('#species-data').html(speciesHtml);
-        $('#gene-data').html(geneHtml);
+        var geneHtml = '';
+        if (result.gene_name) {
+            geneHtml = geneTemplate({ genes: '<a href="' + result.url_gene + '" target="_blank">' + result.gene_name + '</a></p>' });
+        } else {
+            geneHtml = geneTemplate({ genes: '<a href="' + result.url_species + '#data-table" target="_blank">' + result.genes_count + '</a></p>' });
+        }
+
+        $('#species-data').append(speciesHtml);
+        $('#gene-data').append(geneHtml);
     }
 });
--- pcwkb/pcwkb_core/views/search/search_view.py
+++ pcwkb/pcwkb_core/views/search/search_view.py
@@ -6,37 +6,35 @@ from pcwkb_core.models.taxonomy.ncbi_taxonomy import Species
 from pcwkb_core.models.molecular_components.genetic.genes import Gene
 
 def search_pcwkb(request):
-
-    print("chegou no search_pcwkb")
+    print("Entered search_pcwkb view")
     query = request.GET.get('q', '')
 
-    print (query)
+    print(f"Search query: {query}")
 
-    search_results = SearchQuerySet().filter(text=query)
+    search_results = SearchQuerySet().filter(text=query).load_all()
 
-    results={}
+    results = []
     for result in search_results:
+        print(f"Search result: {result}")
+        result_data = {}
         if hasattr(result.object, 'species_code'):
             url_species = f"pcwkb_core/species_page/{result.object.species_code}"
-            results['label']=query
-            results['url']=url_species
-
-            results['specie']=result.object.scientific_name
-            results['genes']=[]
-            if Gene.objects.filter(species=result.object):
-                for gene in Gene.objects.filter(species=result.object):
-                    results['genes'].append(gene.gene_name)
+            result_data['label'] = query
+            result_data['url_species'] = url_species
+            result_data['specie'] = result.object.scientific_name
+            result_data['genes_count'] = Gene.objects.filter(species=result.object).count()
 
         elif hasattr(result.object, 'gene_name'):
             url_gene = f"pcwkb_core/gene_page/{result.object.gene_name}"
-            results['label']=query
-            results['url']=url_gene
+            url_species = f"pcwkb_core/species_page/{result.object.species.species_code}"
+            result_data['label'] = query
+            result_data['url_gene'] = url_gene
+            result_data['url_species'] = url_species
+            result_data['specie'] = result.object.species.scientific_name
+            result_data['gene_name'] = result.object.gene_name
 
-            results['specie']=result.object.species.scientific_name
-            results['genes']=[]
-            results['genes'].append(result.object.gene_name)
+        results.append(result_data)
 
-    print(results)
+    print(f"Results: {results}")
 
     return JsonResponse({'results': results})
-
