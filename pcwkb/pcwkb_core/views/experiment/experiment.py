4c42ccbeb52eb7b4a7c1e964502ed644bbd4b8b1
JNovoletti <joao.novoletti@gmail.com>
2024-06-05 16:52:09 -0300

    updating experiment related data and pages

--- pcwkb/pcwkb_core/views/experiment/experiment.py
+++ pcwkb/pcwkb_core/views/experiment/experiment.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render, get_object_or_404
 
 from pcwkb_core.models.functional_annotation.experimental.experiment import Experiment
+from pcwkb_core.models.functional_annotation.experimental.relationships.biomass_gene_experiment_assoc import BiomassGeneExperimentAssoc
+
+def experiment(request):
+
+    context={"experiments":{}}
+
+    for experiment in Experiment.objects.all():
+        experiment_name = experiment.experiment_name
+        context["experiments"][experiment_name] = {
+            "description": experiment.description,
+            "eco": experiment.eco_term
+        }
+
+    return render(request, 'experiments.html',  context)
 
 def exp_page(request, experiment_name):
+    context = {}
+    
     experiment = get_object_or_404(Experiment, experiment_name=experiment_name)
 
-    print (experiment, experiment.experiment_name)
+    biomass_gene_assocs = experiment.biomassgeneexperimentassoc_set.all()
 
-    return render(request, 'relationships/experiment.html', {'experiment':experiment})
+    if biomass_gene_assocs.exists():
+        assoc_list = []
+
+        for assoc in biomass_gene_assocs:
+            assoc_list.append(assoc)
+
+        context = {
+            "experiment": experiment,
+            "assoc_list": assoc_list,  
+        }
+
+    else:
+        context = {
+            "experiment": experiment,
+            "assoc_list": None, 
+        }
+
+    return render(request, 'relationships/experiment.html', context)
