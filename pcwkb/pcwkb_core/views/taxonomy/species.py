314557592f7f8464e1c8e31daceeb23f20328dec
JNovoletti <joao.novoletti@gmail.com>
2024-06-17 20:11:52 -0300

    adding wiki info to species and cellwallcomp page

--- pcwkb/pcwkb_core/views/taxonomy/species.py
+++ pcwkb/pcwkb_core/views/taxonomy/species.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 from django.core.paginator import Paginator
 from django.http import Http404
 
@@ -44,6 +45,17 @@ def species_page(request, species_code):
 
     context['common_name'] = Species.objects.get(species_code=species_code).common_name
     context['scientific_name'] = Species.objects.get(species_code=species_code).scientific_name
+
+    name=context['scientific_name'].replace(" ","_")
+
+    wiki_url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{name}"
+    response = requests.get(wiki_url)
+    if response.status_code == 200:
+        wikipedia_content = response.json()
+    else:
+        wikipedia_content = {"extract_html": "We are unable to retrieve the Wikipedia information for this entry at this time. Please try again later."}
+
+    context.update({"wikipedia_content": wikipedia_content})
     
     return render(request, 'species/species_page.html', context)
 
