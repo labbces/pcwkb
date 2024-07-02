e87a8e20724aa0ae8244ea65207b473293bc0921
JNovoletti <joao.novoletti@gmail.com>
2024-07-02 16:26:36 -0300

    making intermediary model for ontologies

--- pcwkb/pcwkb_core/models/biomass/cellwall_component.py
+++ pcwkb/pcwkb_core/models/biomass/cellwall_component.py
@@ -8,9 +8,29 @@ class CellWallComponent(models.Model):
     This class stores information about plant cell wall composition, such as its name, 
     the description of the composition and the ChEBI ID for the components, stored in ChEBI class.
     """
-    name = models.CharField(max_length=100)
+    cellwallcomp_name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     chebi = models.ForeignKey(ChEBI, on_delete=models.CASCADE)
+    #kegg = models.ForeignKey(KeggCompound)
 
     def __str__(self):
-        return self.name
+        return self.cellwallcomp_name
+    
+    def add_from_chebi(chebi_name):
+        """ Gets CellWall Component information using the chebi identifier from the chebi model.
+
+        Verify if chebi already exists in the CellWallComponents objects, if not, collect data and store in the fields from this class.
+        """
+
+        try:
+            chebi = ChEBI.objects.get(chebi_name=chebi_name)
+        except:
+            return "chebi_name incorrect"
+        try:
+            cellwall_component = CellWallComponent.objects.get(chebi=chebi)
+            return cellwall_component
+        except:
+            new_cell_wall_component = CellWallComponent.objects.create(name=chebi.chebi_name,
+                                                           description=chebi.definition,
+                                                           chebi=chebi)
+            return new_cell_wall_component
--- pcwkb/pcwkb_core/models/biomass/plant_component.py
+++ pcwkb/pcwkb_core/models/biomass/plant_component.py
@@ -10,7 +10,26 @@ class PlantComponent(models.Model):
     """
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    po = models.ForeignKey(PlantOntologyTerm, on_delete=models.CASCADE)
+    po = models.ManyToManyField(PlantOntologyTerm)
 
     def __str__(self):
         return self.name
+    
+    def add_from_po(po_name):
+        """ Gets CellWall Component information using the po identifier from the po model.
+
+        Verify if po already exists in the CellWallComponents objects, if not, collect data and store in the fields from this class.
+        """
+
+        try:
+            po = PlantOntologyTerm.objects.get(po_name=po_name)
+        except:
+            return "po_name incorrect"
+        try:
+            plant_component = PlantComponent.objects.get(po=po)
+            return f"Plant component already in the db {plant_component}"
+        except:
+            new_plant_component = PlantComponent.objects.create(name=po.po_name,
+                                                           description=po.definition)
+            new_plant_component.po.set([po])
+            return new_plant_component
